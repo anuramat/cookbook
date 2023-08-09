94f96bdc617799b29a40ab24e35315af03de1fc9
Arsen Nuramatov <anuramat@pm.me>
2023-08-09 11:21:46 +0600

    git-filter-repo script

--- exercism_downloader.sh
+++ exercism_downloader.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 exer_dl_track() {
-    local lang="$1"
-    curl -LS "https://exercism.io/tracks/$lang/exercises" |
-        rg "/tracks/$lang/exercises/([\w-]+\w)" -r '$1' -o |
-        xargs -n 1 -P 10 -I{} sh -c "exercism download --track $lang --force  --exercise {} || true" 
+	local lang="$1"
+	curl -LS "https://exercism.io/tracks/$lang/exercises" |
+		rg "/tracks/$lang/exercises/([\w-]+\w)" -r '$1' -o |
+		xargs -n 1 -P 10 -I{} sh -c "exercism download --track $lang --force  --exercise {} || true"
 }
-
-
