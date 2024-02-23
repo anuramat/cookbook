ccf85d8762238a52a601ee070783c87e0f8be77f
Arsen Nuramatov <anuramatov@ozon.ru>
2024-02-23 23:01:57 +0300

    auto

--- exer/patch_haskell.sh
+++ exer/patch_haskell.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-find ~/exercism/haskell -name 'stack.yaml' -exec gsed -i 's/lts-.*/lts-21.25/g' {} +
+find ~/exercism/haskell -name 'stack.yaml' -exec gsed -i 's/resolver:.*/resolver: lts-21.25/g' {} +
