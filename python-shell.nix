ed3eefb6bc79ea0651131ef6495924a9c319774f
Arsen Nuramatov <anuramat@pm.me>
2024-05-04 20:57:46 +0200

    auto

---
+++ python-shell.nix
@@ -0,0 +1,17 @@
+let
+  pkgs = import <nixpkgs> {};
+in
+  pkgs.mkShell {
+    packages = [
+      (pkgs.python3.withPackages (python-pkgs: [
+        python-pkgs.numpy
+        python-pkgs.matplotlib
+        python-pkgs.scikit-image
+        python-pkgs.jupyter
+        python-pkgs.torch
+        python-pkgs.torchvision
+        python-pkgs.pytorch-lightning
+        python-pkgs.jupyter-collaboration
+      ]))
+    ];
+  }
